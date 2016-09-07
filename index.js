c3df27644effaf99059a5a5cac9ea60073309074
fujisawa-shin <fujisawa-shin@kayac.com>
2016-09-08 01:37:15 +0900

    キーワードを覚えてくれる

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
+const fs = require('fs');
 const Botkit = require('botkit');
 
+
 const Config = require('./config.json');
+const DB_FILE = 'db.json';
+
 
 const controller = Botkit.slackbot({
     debug: false
 });
 
+const db = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE)) : {};
+
+function saveDB (cb) {
+    fs.writeFile(DB_FILE, JSON.stringify(db), { encoding: 'utf8' }, cb);
+}
+
 controller.spawn({
     token: Config.SLACK_TOKEN
 }).startRTM();
 
-controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    bot.reply(message, 'Hello yourself.');
+controller.on('direct_mention', (bot, message) => {
+    const text = message.text;
+
+    const matchData = text.match(/^set +([a-z]+) +(.+)$/);
+    if (matchData) {
+        const key = matchData[1];
+        const word = matchData[2];
+        db[key] = word;
+        saveDB((err) => {
+            if (err) {
+                bot.reply(message, `Oh, fail to set word for "${key}".`);
+            } else {
+                bot.reply(message, `ok! I set word for "${key}".`);
+            }
+        });
+    } else if (db[text]) {
+        bot.reply(message, db[text]);
+    } else {
+        bot.reply(message, '?');
+    }
 });
+
+
+
+
+
+
+
+
